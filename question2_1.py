066a55c6b71b20e161bc02eb880a3537400e1db1
manoj1021 <86195560+manoj1021@users.noreply.github.com>
2021-06-20 20:26:21 +0530

    Add files via upload

---
+++ question2_1.py
@@ -0,0 +1,9 @@
+for i in range(0,5):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("\n")
+
+for i in range(5,0,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("\n")
